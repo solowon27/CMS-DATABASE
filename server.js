540210bb720729590424ff7abeddf9317f308d19
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-09 20:44:14 -0700

    sql server connection made successfully

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const mySql = require('mysql');
+const mySql = require('mysql2');
 
 const connection = mySql.createConnection({
   host: 'localhost',
   user: 'root',
+  port: 3306,
   password: 'tj6JxCYk@$',
   database: 'CMS_DB'
 });
