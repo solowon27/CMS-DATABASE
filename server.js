41d42b38e2f8685ddcfcc0b4e05910593b29338c
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-11 02:10:06 -0600

    securing user and password of the app

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const mySql = require('mysql2');
+const sequelize = require('./config/sequelize'); 
+const dotenv = require('dotenv').config();
 
-const connection = mySql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  port: 3306,
-  password: 'tj6JxCYk@$',
-  database: 'CMS_DB'
-});
+const PORT = process.env.PORT || 3001;
 
-connection.connect(function (err) {
-  if (err) throw err;
-  console.log('Connected to CMS database.');
-});
+const connection = mySql.createConnection(
+  {
+    host: '127.0.0.1',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  },
+  console.log(`Connected to the CMS_DB database.`)
+);
+
+// connection.connect(function (err) {
+//   if (err) throw err;
+//   console.log('Connected to the CMS database.');
+// });
 
 module.exports = connection;
