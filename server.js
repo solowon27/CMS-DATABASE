099a85bf28a2ca52a2e7c897cf081254eefc5f6c
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-12 23:48:14 -0600

    adding comments for code review

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const mysql = require('mysql2');
-require('dotenv').config();
-const index = require('./index');
+const mysql = require('mysql2'); //this is mysql2 for our db connection
+require('dotenv').config(); //used for environment variables for secured data
+const index = require('./index'); //this is our index.js file that contains our promptUser function
 
-const connection = mysql.createConnection({
+//we establish our connection to the database on the following lines
+const connection = mysql.createConnection({ 
   host: 'localhost',
-  user: process.env.DB_USER,
+  user: process.env.DB_USER, //drived from .env file
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
 });
 
-connection.connect((err) => {
+connection.connect((err) => { //this is where we connect to the database
   if (err) throw err;
-  console.log('Connected to the CMS_DB database.');
-  index.promptUser(connection);
+  console.log('\n Connected to the CMS_DB database. \n'); //this is a message that will display in the console if we are connected to the database
+  index.promptUser(connection); //this is where we call our promptUser function from our index.js file
 });
 
-module.exports = connection;
+module.exports = connection; //finally we export our connection to be used in other files
